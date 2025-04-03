802cb3cf0637a98640dce4959287743ffb70df0c
DavideCotigliani <davide.cotigliani@gmail.com>
2025-04-03 14:40:45 +0200

    [UPD] Creating the function to generate 10 mails

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 const allList = document.getElementById ('all-list')
 
 //dichiaro le variabili che mi servono
-const number= 10;
 const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail`
-
-
-//svolgo la chiamata AJAX per interrogare l'endpoint
-axios.get(endpoint).then(res =>{
-
-    //recupero l'array dei valori generati
-    const result = res.data.response
-})
+let emailItems;
+//funzione per generare 10 mail random
+const generateEmail = ()=>{
+for(let i= 0; i<10; i++){
+    axios.get(endpoint).then(res=>{
+        const email = res.data.response
+    })
+}
+}
