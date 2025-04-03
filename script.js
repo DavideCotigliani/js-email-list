1faa986399583f4ac981bf93136e2af7179b46dd
DavideCotigliani <davide.cotigliani@gmail.com>
2025-04-03 14:30:31 +0200

    [UPD] Creating the ajax call for the endpoint

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 //recupero gli elementi del dom
 const allList = document.getElementById ('all-list')
-const email = document.getElementById ('email')
+
+//dichiaro le variabili che mi servono
+const number= 10;
+const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail`
+
+
+//svolgo la chiamata AJAX per interrogare l'endpoint
+axios.get(endpoint).then(res =>{
+
+    //recupero l'array dei valori generati
+    const result = res.data.response
+})
