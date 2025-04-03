aa84bf9d7ffa2c56dd90de9996150aaf50effbf3
DavideCotigliani <davide.cotigliani@gmail.com>
2025-04-03 17:09:22 +0200

    [UPD] Updated with the callback function

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-//recupero gli elementi del dom
-const allList = document.getElementById ('all-list')
+// Recupero gli elementi del DOM
+const allList = document.getElementById('all-list');
+const button = document.getElementById('button');
 
-//dichiaro le variabili che mi servono
-const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail`
-//variabile nella quale inserire le mail
+// Dichiaro le variabili che mi servono
+const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail`;
+// Variabile nella quale inserire le mail
 let emailItems = [];
 
-//funzione per generare 10 mail random
-const generateEmail = ()=>{
-    //ciclo for per ciclare i 10 elementi
-for(let i= 0; i<10; i++){
-    //svolgo chiamata AJAX
-    axios.get(endpoint).then(res=>{
-        const email = res.data.response
+// Funzione per generare 10 mail random
+const generateEmail = () => {
+    emailItems = [];  // Resetto l'array prima di iniziare
+    // Ciclo i 10 elementi
+    for (let i = 0; i < 10; i++) {
+        // Svolgo la chiamata AJAX
+        axios.get(endpoint).then(res => {
+                const email = res.data.response;
+                
+                emailItems.push(`<li>${email}</li>`);  // Aggiungo la mail all'array
 
-        //aggiungo gli elementi nell'array emailItems
-        emailItems.push(`<li>${email}</li>`)
-        // una volta raggiunti 10 elementi li unisco insieme
-        if(emailItems.length === 10){
-            allList.innerHTML = emailItems.join('')
-        }
-    })
-}
-}
-//chiamo la funzione
-generateEmail()
+                // Una volta che tutte le email sono state aggiunte, aggiorno il DOM
+                if (emailItems.length === 10) {
+                    allList.innerHTML = emailItems.join('');  // Unisco gli elementi dell'array in una stringa
+                }
+            })
+    }
+};
+// Funzione di callback
+button.addEventListener('click', () => {
+    generateEmail(); 
+});
